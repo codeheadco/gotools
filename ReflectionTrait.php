15069d67a76b908a230c83b21231125a7be94be0
Varga Gábor <gabor87@outlook.com>
2019-08-19 19:03:37 +0200

    Utils docs

--- ReflectionTrait.php
+++ ReflectionTrait.php
@@ -3,41 +3,51 @@
 namespace codeheadco\tools;
 
 /**
- * Description of ReflectionTrait
+ * An OOP util collection as a trait.
  *
  * @author Varga Gábor <gabor87@outlook.com>
  */
 trait ReflectionTrait
 {
 
+    /**
+     * @see Utils::implementsInterface($class, $interfaceClass)
+     */
     public static function implementsInterface($interfaceClass)
     {
         return Utils::implementsInterface(get_called_class(), $interfaceClass);
     }
 
+    /**
+     * @see Utils::usesTrait($class, $traitClass)
+     */
     public static function usesTrait($traitClass)
     {
         return Utils::usesTrait(get_called_class(), $traitClass);
     }
 
+    /**
+     * @see Utils::classShortName($class)
+     */
     public static function classShortName()
     {
         return Utils::classShortName(get_called_class());
     }
 
+    /**
+     * @see Utils::listConstants($class, $groupName, $onlyValues)
+     */
     public static function listConstants($groupName = null, $onlyValues = true)
     {
         return Utils::listConstants(get_called_class(), $groupName, $onlyValues);
     }
     
+    /**
+     * @see Utils::listConstants($class, $groupName, $onlyValues)
+     */
     public static function listProperties($classFilter = null, $onlyNames = false)
     {
         return Utils::listProperties(get_called_class(), $classFilter, $onlyNames);
-    }
-    
-    public static function getAlias($name, $prefix = '')
-    {
-        return Utils::getClassByAlias(get_called_class(), $name, $prefix);
     }
 
 }
--- Utils.php
+++ Utils.php
@@ -3,30 +3,79 @@
 namespace codeheadco\tools;
 
 /**
- * Description of Utils
+ * An OOP util collection.
  *
  * @author Varga Gábor <gabor87@outlook.com>
  */
 class Utils
 {
 
+    /**
+     * Checks if the $class does implement the $interfaceClass.
+     * @param string|object $class
+     * @param string $interfaceClass
+     * @return boolean
+     */
     public static function implementsInterface($class, $interfaceClass)
     {
         $interfaces = class_implements($class);
         return isset($interfaces[$interfaceClass]);
     }
+    
+    /**
+     * @see \ReflectionClass::isSubclassOf($class)
+     * @param string|object $class
+     * @param string|object $compareTo
+     * @return boolean
+     */
+    public static function isSubclassOf($class, $compareTo)
+    {
+        return (new \ReflectionClass($class))->isSubclassOf($compareTo);
+    }
+    
+    /**
+     * Checks if the $class is an instance of the $compareTo. (The same as the "instanceof" operator but static.)
+     * @param string $class
+     * @param string $compareTo
+     * @return boolean
+     */
+    public static function isInstanceOf(string $class, string $compareTo)
+    { 
+        return ltrim($class, '\\') === ltrim($compareTo, '\\') || static::isSubclassOf($class, $compareTo);
+    }
 
-    public static function usesTrait($class, $traitClass)
+    /**
+     * Checks if the $class uses the $traitClass as trait.
+     * @param string|object $class
+     * @param string $traitClass
+     * @return boolean
+     */
+    public static function usesTrait($class, string $traitClass)
     {
         $traits = class_uses($class);
         return isset($traits[$traitClass]);
     }
 
+    /**
+     * @see \ReflectionClass::getShortName()
+     * @param string|object $class
+     * @return string
+     */
     public static function classShortName($class)
     {
         return (new \ReflectionClass($class))->getShortName();
     }
 
+    /**
+     * Returns by an array of the $class constants,
+     * if the $groupName is null, then the all constants or else filters by that,
+     * if the $onlyValues is true, then only the constants' values or else a map as: "constant name" => "value name".
+     * @param string|object $class
+     * @param string $groupName
+     * @param boolean $onlyValues
+     * @return array()
+     * @throws \InvalidArgumentException
+     */
     public static function listConstants($class, $groupName = null, $onlyValues = true)
     {
         $constants = (new \ReflectionClass($class))->getConstants();
@@ -48,7 +97,17 @@ class Utils
         return $onlyValues ? array_values($constants) : $constants;
     }
     
-    public static function listProperties($class, $classFilter = null, $onlyNames = false)
+    /**
+     * Returns by an array of the $class properties,
+     * if the $classFilter is null, then the all properties from the inheritance tree or else filters by that,
+     * if the $onlyNames is false, then an array of \ReflectionProperty instances or else only property names.
+     * @see \ReflectionClass::getProperties()
+     * @param string|object $class
+     * @param string $classFilter
+     * @param boolean $onlyNames
+     * @return array()
+     */
+    public static function listProperties($class, string $classFilter = null, bool $onlyNames = false)
     {
         $properties = (new \ReflectionClass($class))->getProperties();
         
